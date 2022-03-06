c9bb345915329c6691d4929450581be9f2f09fa2
Chebrolu Tejaswi <tejaswichebrolu@gmail.com>
2022-03-06 23:32:34 +0530

    modified settings.py

--- farmfully/settings.py
+++ farmfully/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-na0pn28t8=j=^(bn0y4!!#uy+jcw069iyd5sa9%@sulan-sfw7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["farm-fully.herokuapp.com",'localhost','127.0.0.1:8000']
 
 
 # Application definition
